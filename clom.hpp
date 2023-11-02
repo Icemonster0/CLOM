e9d51db3c6708f06c3a43bb1fb3727bede874338
Andras <andras.borsanyi09@gmail.com>
2023-11-02 22:25:02 +0100

    Temporary commit

--- clom.hpp
+++ clom.hpp
@@ -28,17 +28,28 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <typeinfo>
+#include <memory>
 
-struct CLOM_Setting {
-    CLOM_Setting(std::string p_name, std::string p_value) : name(p_name), value(p_value) {
+struct CLOM_General_Setting {
+    CLOM_General_Setting(std::string p_name) : name(p_name) {
     }
 
     std::string name;
-    std::string value;
+};
+
+template<typename T>
+struct CLOM_Setting : public CLOM_General_Setting {
+    CLOM_Setting(std::string p_name, T p_value)
+        : CLOM_General_Setting {p_name}, value(p_value) {
+    }
+
+    T value;
 };
 
 struct CLOM_Flag {
-    CLOM_Flag(std::string p_name) : name(p_name), is_set(false) {
+    CLOM_Flag(std::string p_name)
+        : name(p_name), is_set(false) {
     }
 
     std::string name;
@@ -51,8 +62,9 @@ public:
     CL_Option_Manager() : user_hint("Invalid command line options!\n") {
     }
 
-    void register_setting(std::string name, std::string default_value) {
-        settings.emplace_back(name, default_value);
+    template<typename T>
+    void register_setting(std::string name, T default_value) {
+        settings.push_back(new CLOM_Setting<T>(name, default_value));
     }
 
     void register_flag(std::string name) {
@@ -61,11 +73,21 @@ public:
 
     void process_cl_options(int argc, char const *argv[]) {
         for (size_t i = 1; i < argc;) {
-            CLOM_Setting *setting = get_setting_by_name(argv[i]);
+            CLOM_Setting<int> *int_setting = get_setting_by_name<int>(argv[i]);
+            CLOM_Setting<float> *float_setting = get_setting_by_name<float>(argv[i]);
+            CLOM_Setting<std::string> *string_setting = get_setting_by_name<std::string>(argv[i]);
             CLOM_Flag *flag = get_flag_by_name(argv[i]);
 
-            if (setting != nullptr && i < argc-1) {
-                setting->value = argv[i+1];
+            if (int_setting != nullptr && i < argc-1) {
+                int_setting->value = std::stoi(argv[i+1]);
+                i += 2;
+            }
+            else if (float_setting != nullptr && i < argc-1) {
+                float_setting->value = std::stof(argv[i+1]);
+                i += 2;
+            }
+            else if (string_setting != nullptr && i < argc-1) {
+                string_setting->value = argv[i+1];
                 i += 2;
             }
             else if (flag != nullptr) {
@@ -80,8 +102,9 @@ public:
         }
     }
 
-    std::string get_setting_value(std::string name) {
-        CLOM_Setting *setting = get_setting_by_name(name);
+    template<typename T>
+    T get_setting_value(std::string name) {
+        CLOM_Setting<T> *setting = get_setting_by_name<T>(name);
 
         if (setting != nullptr)
             return setting->value;
@@ -112,10 +135,12 @@ public:
 
 private:
 
-    CLOM_Setting* get_setting_by_name(std::string name) {
-        for (CLOM_Setting &setting : settings) {
-            if (setting.name.compare(name) == 0) {
-                return &setting;
+    template<typename T>
+    CLOM_Setting<T>* get_setting_by_name(std::string name) {
+        for (std::unique_ptr<CLOM_General_Setting> setting : settings) {
+            if (typeid(T) == typeid(setting->get_value()) &&
+                setting->name.compare(name) == 0) {
+                return setting.get();
             }
         }
 
@@ -132,7 +157,7 @@ private:
         return nullptr;
     }
 
-    std::vector<CLOM_Setting> settings;
+    std::vector<std::unique_ptr<CLOM_General_Setting>> settings;
     std::vector<CLOM_Flag> flags;
 
     std::string user_hint;
---
+++ demo.cpp
@@ -0,0 +1,36 @@
+/*
+ * This demonstration shows how one could use the CLOM library.
+ * In this case, we can set the name, height and smartness
+ * of our subject using command line options.
+ * Example:
+ * $ MyApp name Mark height 5.2 --smart
+ * Result:
+ * Mark is 5.2 foot tall and is smart.
+*/
+
+#include "clom.hpp"
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+
+    // create the clom object
+    CL_Option_Manager clom;
+
+    // 1. register settings (with default values) and flags
+    clom.register_setting<std::string>("name", CLOM_Type::STRING, "Mr X");
+    clom.register_setting<float>("height", CLOM_Type::FLOAT, 6.0f);
+    clom.register_flag("--smart");
+
+    // 2. let clom process the options
+    clom.process_cl_options(argc, argv);
+
+    // 3. save and print the settings
+    std::string name = clom.get_setting_value<std::string>("name");
+    float height = clom.get_setting_value<float>("height");
+    bool is_smart = clom.is_flag_set("--smart");
+
+    std::cout << name << " is " << height << " foot tall ";
+    std::cout << "and is " << (is_smart ? "smart." : "not smart.") << '\n';
+
+    return 0;
+}
